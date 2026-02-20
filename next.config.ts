9dd95eda5112cff5ce82a694de23762060b343fe
50000000000000000000000 <chukwunebumsimeon@gmail.com>
2026-02-20 14:29:18 +0100

    Firebase CSP + admin override fixes

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://translate.google.com https://www.gstatic.com; img-src 'self' data: https://fonts.gstatic.com https://www.gstatic.com https://www.google.com https://translate.googleapis.com; font-src 'self' https://fonts.gstatic.com; frame-src 'self' https://translate.google.com; connect-src 'self' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com; upgrade-insecure-requests",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com https://www.gstatic.com https://www.googleapis.com https://www.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://translate.google.com https://www.gstatic.com; img-src 'self' data: https://fonts.gstatic.com https://www.gstatic.com https://www.google.com https://translate.googleapis.com; font-src 'self' https://fonts.gstatic.com; frame-src 'self' https://translate.google.com; connect-src 'self' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com https://identitytoolkit.googleapis.com https://firestore.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com; upgrade-insecure-requests",
           },
         ],
       },
